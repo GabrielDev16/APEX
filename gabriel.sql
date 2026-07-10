8ab4a966417c6e8ae30f4d951e4f87e9a9e13eea
Gabriel <Gabriel@gmail.com>
2026-07-10 18:06:11 -0300

    adcionando o meu banco de dados

---
+++ gabriel.sql
@@ -0,0 +1,282 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 18, 2026 at 11:00 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gabriel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `boks`
+--
+
+CREATE TABLE `boks` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `autor` varchar(255) NOT NULL,
+  `total_paginas` int(11) NOT NULL,
+  `paginas_lidas` int(11) DEFAULT 0,
+  `capa` varchar(50) DEFAULT 'NULL',
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `boks`
+--
+
+INSERT INTO `boks` (`id`, `titulo`, `autor`, `total_paginas`, `paginas_lidas`, `capa`, `data_cadastro`) VALUES
+(1, 'PHP 8', 'Paulo A.', 675, 65, 'capa php.jpeg', '2026-06-17 15:22:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `habitos`
+--
+
+CREATE TABLE `habitos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `meta_mensal` int(11) NOT NULL,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `habitos`
+--
+
+INSERT INTO `habitos` (`id`, `titulo`, `descricao`, `meta_mensal`, `criado_em`) VALUES
+(1, 'Ler 20 Páginas por dia', NULL, 31, '2026-06-17 19:28:34'),
+(2, 'Fazer os deveres de casa', NULL, 30, '2026-06-17 19:33:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meses`
+--
+
+CREATE TABLE `meses` (
+  `id` int(11) NOT NULL,
+  `nome_mes` varchar(20) NOT NULL,
+  `ano` int(11) NOT NULL,
+  `total_dias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `metas`
+--
+
+CREATE TABLE `metas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `titulo` varchar(45) DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `data_criacao` datetime DEFAULT current_timestamp(),
+  `data_limite` date DEFAULT NULL,
+  `status` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `metas`
+--
+
+INSERT INTO `metas` (`id`, `titulo`, `descricao`, `data_criacao`, `data_limite`, `status`) VALUES
+(15, 'Conseguir Um Trabalho', 'Como proficional de T.I.', '2026-06-17 14:36:44', '2026-07-15', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `Perfil` varchar(50) DEFAULT 'NULL'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `progresso_habitos`
+--
+
+CREATE TABLE `progresso_habitos` (
+  `id` int(11) NOT NULL,
+  `habito_mes_id` int(11) NOT NULL,
+  `dia` int(11) NOT NULL,
+  `status` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registros_habito`
+--
+
+CREATE TABLE `registros_habito` (
+  `id` int(11) NOT NULL,
+  `habito_id` int(11) DEFAULT NULL,
+  `data_registro` date DEFAULT NULL,
+  `concluido` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `registros_habito`
+--
+
+INSERT INTO `registros_habito` (`id`, `habito_id`, `data_registro`, `concluido`) VALUES
+(1, 1, '2026-06-17', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transacoes`
+--
+
+CREATE TABLE `transacoes` (
+  `id` int(11) NOT NULL,
+  `tipo` enum('entrada','saida') NOT NULL,
+  `categoria` varchar(50) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `data_lancamento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `boks`
+--
+ALTER TABLE `boks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `habitos`
+--
+ALTER TABLE `habitos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meses`
+--
+ALTER TABLE `meses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `metas`
+--
+ALTER TABLE `metas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `perfil`
+--
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `progresso_habitos`
+--
+ALTER TABLE `progresso_habitos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registros_habito`
+--
+ALTER TABLE `registros_habito`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `habito_id` (`habito_id`);
+
+--
+-- Indexes for table `transacoes`
+--
+ALTER TABLE `transacoes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `boks`
+--
+ALTER TABLE `boks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `habitos`
+--
+ALTER TABLE `habitos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `meses`
+--
+ALTER TABLE `meses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `metas`
+--
+ALTER TABLE `metas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `perfil`
+--
+ALTER TABLE `perfil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `progresso_habitos`
+--
+ALTER TABLE `progresso_habitos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registros_habito`
+--
+ALTER TABLE `registros_habito`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transacoes`
+--
+ALTER TABLE `transacoes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `registros_habito`
+--
+ALTER TABLE `registros_habito`
+  ADD CONSTRAINT `registros_habito_ibfk_1` FOREIGN KEY (`habito_id`) REFERENCES `habitos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
